fbe7dd68cf9dfef95b9e2b0fc1e172ee4b1914a6
asalontai <93924842+asalontai@users.noreply.github.com>
2024-07-26 15:38:54 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,15 +2,15 @@ export const geo_api = {
   method: 'GET',
   url: 'https://wft-geo-db.p.rapidapi.com/v1/geo',
   headers: {
-    'X-RapidAPI-Key': '52d914c11emsha442a429be7d438p12a379jsn58ecd73ab6f5',
-    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    'X-RapidAPI-Key': '',
+    'X-RapidAPI-Host': ''
   }
 };
 
-export const WEATHER_API_KEY = 'e13e773c69f51c23e102409550e96faa';
+export const WEATHER_API_KEY = '';
 
 export const weather_api_current = 'https://api.openweathermap.org/data/2.5/weather'
 
-export const FORCAST_API_KEY = 'b92a0807207c453da5f191634242601'
+export const FORCAST_API_KEY = ''
 
 export const forcast_api = 'https://api.weatherapi.com/v1/forecast.json'
